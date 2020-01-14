10594ab871567ca48af3f19054cf3d9e0abe9e9d
AivarsB <aivars.berzins@inbox.lv>
2020-01-14 17:23:55 +0200

    Add project files.

---
+++ 45.uzd Hello World n reizes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _45.uzd_Hello_World_n_reizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of times to repeat 'Hello World': ");
+            int number;
+            bool result = int.TryParse(Console.ReadLine(), out number);
+
+            if (result)
+            {
+                for (int i = 0; i < number; i++)
+                {
+                    Console.WriteLine("Hello World");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+
+        }
+    }
+}
+// Izveidot console aplikāciju, kura lietotājam paprasīs, cik reizes ir jāatkārto for cikls.
